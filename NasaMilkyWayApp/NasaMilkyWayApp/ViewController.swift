ee1827962b37899d8f3c8a7090e2588b8a27ce8c
Emese Tóth <temsethebest@gmail.com>
2018-11-04 23:04:00 +0000

    Third party libraries

---
+++ NasaMilkyWayApp/NasaMilkyWayApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NasaMilkyWayApp
+//
+//  Created by Emese Toth on 04/11/2018.
+//  Copyright © 2018 Emese Toth. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
